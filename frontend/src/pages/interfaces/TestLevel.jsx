e2e18831db48627dd49e02485e92cc1e6f27f448
bhosalesooham5713@gmail.com <bhosalesoham5713@gmail.com>
2024-12-12 00:58:20 +0530

    added login page, home page, layout, dialogues page, prelevels page

---
+++ frontend/src/pages/interfaces/TestLevel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LayoutPage from './LayoutPage';
+
+const TestLevel = () => {
+    return (
+        <div className='flex justify-center items-center h-screen relative'>
+            <LayoutPage />
+            <img
+                src="/src/assets/interfaces/TestLevel.png"
+                alt="Background"
+                className='object-cover w-full h-full absolute z-0'
+            />
+            <div className='absolute'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TestLevel
