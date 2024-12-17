3dd0530f4c062619b26a8a92f7e78601eb14a96c
bhosalesooham5713@gmail.com <bhosalesoham5713@gmail.com>
2024-12-17 22:47:07 +0530

    levels added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,22 +16,22 @@ import Level_2 from './pages/levels/Level_2';
 import Backstory_Level_3 from './pages/backstory/Backstory_Level_3';
 import Level_3 from './pages/levels/Level_3';
 import Backstory_Level_4 from './pages/backstory/Backstory_Level_4';
-import Options_Level_4 from './pages/options/Options_Level_4'; // Koi toh karlo
-import Dialogues_Level_4A from './pages/dialogues/Dialogues_Level_4A'; // Soham Bhosale
-import Level_4A from './pages/levels/Level_4A'; // Yash Dharamshi
+// import Options_Level_4 from './pages/options/Options_Level_4'; // Koi toh karlo
+// import Dialogues_Level_4A from './pages/dialogues/Dialogues_Level_4A'; // Soham Bhosale
+// import Level_4A from './pages/levels/Level_4A'; // Yash Dharamshi
 import Backstory_Level_5A from './pages/backstory/Backstory_Level_5A';
-import Level_5_1 from './pages/levels/Level_5_1'; // Soham Bhosale
+// import Level_5_1 from './pages/levels/Level_5_1'; // Soham Bhosale
 import Backstory_Level_5B from './pages/backstory/Backstory_Level_5B';
-import Options_Level_5B from './pages/options/Options_Level_5B'; // Koi toh karlo
-import Level_5_2A from './pages/levels/Level_5_2A'; // Tattva Jain
+// import Options_Level_5B from './pages/options/Options_Level_5B'; // Koi toh karlo
+// import Level_5_2A from './pages/levels/Level_5_2A'; // Tattva Jain
 import Level_6_1 from './pages/levels/Level_6_1';
 import Level_6_2 from './pages/levels/Level_6_2'; 
-import Level_7_1 from './pages/levels/Level_7_1'; // Aayush Kolte
-import Options_Level_7A from './pages/options/Options_Level_7A'; // Koi toh karlo
+// import Level_7_1 from './pages/levels/Level_7_1'; // Aayush Kolte
+// import Options_Level_7A from './pages/options/Options_Level_7A'; // Koi toh karlo
 import Level_7_1A from './pages/levels/Level_7_1A';
-import Level_7_1B from './pages/levels/Level_7_1B'; // Sanyogeeta Pradhan - RGB code remaining
-import Level_7_3 from './pages/levels/Level_7_3'; // Vinisha Bhagwani
-import Level_7_4 from './pages/levels/Level_7_4'; // Akshat Patil
+// import Level_7_1B from './pages/levels/Level_7_1B'; // Sanyogeeta Pradhan - RGB code remaining
+// import Level_7_3 from './pages/levels/Level_7_3'; // Vinisha Bhagwani
+// import Level_7_4 from './pages/levels/Level_7_4'; // Akshat Patil
 
 const App = () => {
   return (
@@ -52,22 +52,22 @@ const App = () => {
         <Route path="/backstory_level_3" element={<Backstory_Level_3 />} />
         <Route path="/level_3" element={<Level_3 />} />
         <Route path="/backstory_level_4" element={<Backstory_Level_4 />} />
-        <Route path="/options_level_4" element={<Options_Level_4 />} />
-        <Route path="/dialogues_level_4A" element={<Dialogues_Level_4A />} />
-        <Route path="/level_4A" element={<Level_4A />} />
+        {/* <Route path="/options_level_4" element={<Options_Level_4 />} /> */}
+        {/* <Route path="/dialogues_level_4A" element={<Dialogues_Level_4A />} /> */}
+        {/* <Route path="/level_4A" element={<Level_4A />} /> */}
         <Route path="/backstory_level_5A" element={<Backstory_Level_5A />} />
-        <Route path="/level_5_1" element={<Level_5_1 />} />
+        {/* <Route path="/level_5_1" element={<Level_5_1 />} /> */}
         <Route path="/backstory_level_5B" element={<Backstory_Level_5B />} />
-        <Route path="/options_level_5B" element={<Options_Level_5B />} />
-        <Route path="/level_5_2A" element={<Level_5_2A />} />
+        {/* <Route path="/options_level_5B" element={<Options_Level_5B />} /> */}
+        {/* <Route path="/level_5_2A" element={<Level_5_2A />} /> */}
         <Route path="/level_6_1" element={<Level_6_1 />} />
         <Route path="/level_6_2" element={<Level_6_2 />} />
-        <Route path="/level_7_1" element={<Level_7_1 />} />
-        <Route path="/options_level_7A" element={<Options_Level_7A />} />
+        {/* <Route path="/level_7_1" element={<Level_7_1 />} /> */}
+        {/* <Route path="/options_level_7A" element={<Options_Level_7A />} /> */}
         <Route path="/105_99_104_111_114_102_111_110_105_97_115" element={<Level_7_1A />} />
-        <Route path="/level_7_1B" element={<Level_7_1B />} />
-        <Route path="/level_7_3" element={<Level_7_3 />} />
-        <Route path="/level_7_4" element={<Level_7_4 />} />
+        {/* <Route path="/level_7_1B" element={<Level_7_1B />} /> */}
+        {/* <Route path="/level_7_3" element={<Level_7_3 />} /> */}
+        {/* <Route path="/level_7_4" element={<Level_7_4 />} /> */}
       </Routes>
     </Router>
   )
