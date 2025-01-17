ae5c97b4e0062c7484aeae7e8bd6f8336f2b9d87
Aaryan <aaryanman05@gmail.com>
2025-01-17 16:35:30 +0000

    Level 5 push

--- wizard-gameplay/src/main.jsx
+++ wizard-gameplay/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
-import Level5 from './pages/levels/Level5.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="levels">
-          <Route path="level_5" element={<Level5 />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
