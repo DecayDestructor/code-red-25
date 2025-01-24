f4c2faac959f9f547ea99ea666cdafef844850b1
Varun J <vnjhaveri@gmail.com>
2025-01-24 20:56:40 +0530

    level 6 completed

--- wizard-gameplay/src/main.jsx
+++ wizard-gameplay/src/main.jsx
@@ -8,7 +8,7 @@ import Level4 from './Level4.jsx';
 import Level3 from './Level3.jsx';
 import Level2 from './Level2.jsx';
 import Level1 from './Level1.jsx';
-
+import Level6 from './Level6.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -20,6 +20,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/level2" element={<Level2 />} />
         <Route path="/level3" element={<Level3 />} />
         <Route path="/level4" element={<Level4 />} />
+        <Route path="/level6" element={<Level6 />} />
         <Route path="levels">
           
         </Route>
