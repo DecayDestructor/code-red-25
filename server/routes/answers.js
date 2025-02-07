e6ec7a47c914f9f37cca83a5fa87d354b629be4a
Decay_Destructor <aaryanmantri29@gmail.com>
2025-02-07 23:43:50 +0530

    caching teams

--- server/routes/answers.js
+++ server/routes/answers.js
@@ -128,6 +128,49 @@ router.get('/check-answer/warrior/:level', async (req, res) => {
   }
 })
 
+router.get('check-answer/wizard/:level', async (req, res) => {
+  const { level } = req.params
+  const { answer, teamId } = req.body
+  const redisKey = `wizard:level:${level}`
+  const teamKey = `team:${teamId}`
+  try {
+    const cachedData = await redis.get(redisKey)
+    if (cachedData) {
+      console.log('Serving from Redis Cache')
+      const result = JSON.parse(cachedData)
+      console.log(result)
+      const isCorrect = result.some(
+        (row) => row.answer.toLowerCase() === answer.toLowerCase()
+      )
+      if (isCorrect) {
+        await redis.hset(teamKey, 'wizard:level', level)
+        console.log(await redis.hgetall(teamKey))
+        return res.status(200).json({ correct: true })
+      } else {
+        return res.status(200).json({ correct: false })
+      }
+    }
+    const answerQuery = 'SELECT * FROM wizard_level WHERE level= $1 '
+    const result = await pool.query(answerQuery, [level])
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Level not found' })
+    }
+    await redis.set(redisKey, JSON.stringify(result.rows), 'EX', 300)
+    const isCorrect = result.rows.some(
+      (row) => row.answer.toLowerCase() === answer.toLowerCase()
+    )
+    if (isCorrect) {
+      await redis.hset(teamKey, 'wizard:level', level)
+      console.log(await redis.hgetall(teamKey))
+      return res.status(200).json({ correct: true })
+    } else {
+      return res.status(200).json({ correct: false })
+    }
+  } catch (err) {
+    console.error('Error fetching data:', err)
+    return res.status(500).send('Server Error')
+  }
+})
 // route to check if an answer is correct
 
 module.exports = router
