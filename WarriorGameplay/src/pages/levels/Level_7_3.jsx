564b53769793a0c342cd26aa4b8df9056a3633a0
RohitMangale <119059546+RohitMangale@users.noreply.github.com>
2025-02-14 11:18:45 +0530

    double imports fixed

--- WarriorGameplay/src/pages/levels/Level_7_3.jsx
+++ WarriorGameplay/src/pages/levels/Level_7_3.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LayoutPage from '../interfaces/LayoutPage';
-import { useDispatch} from 'react-redux'
-import { unlockLevel  } from '../../store';
+
 import { useDispatch} from 'react-redux'
 import { unlockLevel  } from '../../store';
 
