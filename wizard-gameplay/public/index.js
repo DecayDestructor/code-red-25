52cd388d70efb11cdf55bf55c36aab5d50ae642a
Aaryan <aaryanman05@gmail.com>
2025-02-15 14:21:59 +0000

    Basic level 5 answer auth template

---
+++ wizard-gameplay/public/index.js
@@ -0,0 +1,22 @@
+const submitBtn = document.querySelector(".ans_btn")
+// console.log(submitBtn);
+
+submitBtn.addEventListener("click" , async(e) =>{
+    const answer1 = document.querySelector(".ans_1")
+    const answer2 = document.querySelector(".ans_2")
+    const answer3 = document.querySelector(".ans_3")
+
+    /////Sending the post request to the API//////
+    const id = localStorage.getItem('id') || 1
+    const response = await fetch(`answers/check-answer/wizard/5`,{
+        method:'POST',
+        body:{
+            teamId: id,
+            //The elements of the answer array should be in the same order:
+            //answer1 , answer2 and then answer3 
+            answer: [answer1 , answer2 , answer3] 
+            
+        }
+    })
+
+})
