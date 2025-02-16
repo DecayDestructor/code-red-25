302bcefa005c8a4bdcd9e979d8aa2e71bef7d85e
Aaryan <aaryanman05@gmail.com>
2025-02-16 06:10:58 +0000

    Experimental checker

--- wizard-gameplay/public/index.js
+++ wizard-gameplay/public/index.js
@@ -8,15 +8,33 @@ submitBtn.addEventListener("click" , async(e) =>{
 
     /////Sending the post request to the API//////
     const id = localStorage.getItem('id') || 1
-    const response = await fetch(`answers/check-answer/wizard/5`,{
-        method:'POST',
-        body:{
-            teamId: id,
-            //The elements of the answer array should be in the same order:
-            //answer1 , answer2 and then answer3 
-            answer: [answer1 , answer2 , answer3] 
-            
+    try{
+
+        const response = await fetch(`answers/check-answer/wizard/5`,{
+            method:'POST',
+            body:{
+                teamId: id,
+                //The elements of the answer array should be in the same order:
+                //answer1 , answer2 and then answer3 
+                answer: [answer1 , answer2 , answer3] 
+                
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
         }
-    })
+        
+        const json = await response.json();
+
+        //Routing to next level is answer is correct otherwise not
+        if (json){
+            window.location.href = "/level6"
+        }
+
+    } catch(e){
+        console.log(`Error while sending request : ${e}`);
+        
+    }
 
 })
