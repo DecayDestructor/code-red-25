6c1acab3ba320ae1b9813d12473379961cac1cc5
Varun J <85669579+CYCLOP5@users.noreply.github.com>
2025-02-19 07:44:34 +0000

    fix answer

--- server/data/wizard-answers.js
+++ server/data/wizard-answers.js
@@ -1,7 +1,7 @@
 const wizardAnswers = {
   1: ['79.06', '79', '80'],
   2: ['3', '12', '15'], // Slider pe yeh values set
-  3: ['3122113112221'],
+  3: ['31221113112221'],
   4: ['0x2804CDF61b998278124118f1E2C291bd0aD70833'],
   5: [
     'efc6c8c8de1929382723cc31814a1880119e6f8b32b52b910ec0b84e7920d6b1ANSWER-2-HEHEf9088bf8e4d49bdaada41c7c3ba691f4b345af3d520f7f05e487dceb1e81c429',
