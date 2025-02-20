42d4e97e076ec17421b03caa5266f89f328883fd
Rohit <119059546+RohitMangale@users.noreply.github.com>
2025-02-20 22:52:25 +0530

    level 5a added

--- WarriorGameplay/src/pages/levels/Level_4A.jsx
+++ WarriorGameplay/src/pages/levels/Level_4A.jsx
@@ -21,6 +21,7 @@ const CombinedComponent = () => {
       if (correct) {
         setResultMessage('Correct! Well done!')
         setTimeout(() => {
+          dispatch(unlockLevel('level_5a'))
           dispatch(unlockLevel('level_6_1'))
           dispatch(lockLevel('options_level_4'))
           navigate('/backstory_level_5a')
